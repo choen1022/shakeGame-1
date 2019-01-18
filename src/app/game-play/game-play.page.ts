210d65b96a3c3504d71f77c6ecf7c051e4f8be0a
Suthon <suthon@gmail.com>
2019-01-18 15:46:44 +0700

    game begin

---
+++ src/app/game-play/game-play.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-play',
+  templateUrl: './game-play.page.html',
+  styleUrls: ['./game-play.page.scss'],
+})
+export class GamePlayPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/slot-picker/slot-picker.page.ts
+++ src/app/slot-picker/slot-picker.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../game.service';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-slot-picker',
@@ -9,13 +10,30 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SlotPickerPage implements OnInit {
   team: number
+  slots = {}
   constructor(
     private route: ActivatedRoute,
-    private gameService: GameService
+    private gameService: GameService,
+    private db: AngularFireDatabase
   ) { }
 
   ngOnInit() {
     this.team = parseInt(this.route.snapshot.paramMap.get('team'));
+    this.db.object(`rooms/default/${this.team == 1 ? 'blue' : 'red'}/slots`)
+      .valueChanges().subscribe(slots => this.slots = slots)
+  }
+
+  joinGame(num){
+    this.db.object(`rooms/default/${this.team == 1 ? 'blue' : 'red'}/slots/slot${num}`)
+      .set(this.gameService.uuid);
+    this.db.object(`rooms/default/_state`)
+      .valueChanges().subscribe((_state : any) => {
+        if(_state.play){
+          this.gameService.team = this.team
+          this.gameService.slot = num
+          console.log('Game Begin!')
+        }
+      })
   }
 
 }
